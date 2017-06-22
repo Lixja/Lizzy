dd386e2e33b5737e97c33294cdcc7427065571e0
Dimitrios Diamantidis <Dimitri.dia@ledimi.com>
2017-06-22 13:26:18 +0200

    Added translation from int to string(ascii).

--- liz/liz.go
+++ liz/liz.go
@@ -1,8 +1,11 @@
 package main
 
-import "Lizzy/brain"
-import "fmt"
-import "strconv"
+import (
+	"LizzyAI/brain"
+	util "LizzyAI/utils"
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	liz := brain.GenerateBrain(5, 5, 2, 7)
@@ -15,26 +18,27 @@ func main() {
 	write("76 - " + strconv.Itoa(Converted[2]))
 	write("76 - " + strconv.Itoa(Converted[3]))
 	write("79 - " + strconv.Itoa(Converted[4]))
-	liz.Train(0.001, Inputs, Inputs, 100000, true)
+	write(util.GetAsciiC(Converted[0]) + util.GetAsciiC(Converted[1]) + util.GetAsciiC(Converted[2]) + util.GetAsciiC(Converted[3]) + util.GetAsciiC(Converted[4]))
+	liz.Train(0.0001, Inputs, Inputs, 100000, true)
 	Outputs = nil
 	Outputs = liz.Think(Inputs)
 	Converted = []int{int(Outputs[0]), int(Outputs[1]), int(Outputs[2]), int(Outputs[3]), int(Outputs[4])}
-	write("")
 	write("72 - " + strconv.Itoa(Converted[0]))
 	write("65 - " + strconv.Itoa(Converted[1]))
 	write("76 - " + strconv.Itoa(Converted[2]))
 	write("76 - " + strconv.Itoa(Converted[3]))
 	write("79 - " + strconv.Itoa(Converted[4]))
-	liz.Train(0.0001, Inputs, Inputs, 1000000, false)
+	write(util.GetAsciiC(Converted[0]) + util.GetAsciiC(Converted[1]) + util.GetAsciiC(Converted[2]) + util.GetAsciiC(Converted[3]) + util.GetAsciiC(Converted[4]))
+	liz.Train(0.001, Inputs, Inputs, 10000000, false)
 	Outputs = nil
 	Outputs = liz.Think(Inputs)
 	Converted = []int{int(Outputs[0]), int(Outputs[1]), int(Outputs[2]), int(Outputs[3]), int(Outputs[4])}
-	write("")
 	write("72 - " + strconv.Itoa(Converted[0]))
 	write("65 - " + strconv.Itoa(Converted[1]))
 	write("76 - " + strconv.Itoa(Converted[2]))
 	write("76 - " + strconv.Itoa(Converted[3]))
 	write("79 - " + strconv.Itoa(Converted[4]))
+	write(util.GetAsciiC(Converted[0]) + util.GetAsciiC(Converted[1]) + util.GetAsciiC(Converted[2]) + util.GetAsciiC(Converted[3]) + util.GetAsciiC(Converted[4]))
 }
 
 func positive(i int) int {
